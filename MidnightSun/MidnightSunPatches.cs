a20f14ad9b434b7c83bb59a50f331564b731194e
Yannick <yannick@yannickschmitt.de>
2020-11-06 01:55:07 +0100

    Initial release for Midnight Sun

---
+++ MidnightSun/MidnightSunPatches.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using Klei.AI;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace MidnightSun
+{
+    // Midnight Sun (Eternal Daylight) mod for Oxygen not included
+    // By Yannick M. Schmitt
+    public class MidnightSunPatches
+    {
+        [HarmonyPatch(typeof(TimeOfDay), "UpdateVisuals")]
+        public static class TimeOfDay_UpdateVisuals_Patch
+        {
+            public static void Postfix()
+            {
+                Shader.SetGlobalVector("_TimeOfDay", new Vector4(0.0f, 1f, 0.0f, 0.0f));
+            }
+        }
+    }
+}
